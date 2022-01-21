b0748cb4b1caba4212c394ad46163be9121da75a
savrasovmv <savrasovmv@tmenergo.ru>
2022-01-21 17:36:25 +0500

    redirect исправлен

--- addons/it_base/controllers/auth_main.py
+++ addons/it_base/controllers/auth_main.py
@@ -1,6 +1,6 @@
 from odoo import http, _
 from odoo.addons.auth_signup.controllers.main import AuthSignupHome
-
+import werkzeug
 from odoo.http import request
 
 
@@ -8,8 +8,8 @@ class ItAuthSignupHome(AuthSignupHome):
 
     @http.route('/web/reset_password', type='http', auth='public', website=True, sitemap=False)
     def web_auth_reset_password(self, *args, **kw):
-        return request.redirect('/') 
+        return  werkzeug.utils.redirect('/') #request.redirect('/') 
 
     @http.route('/web/signup', type='http', auth='public', website=True, sitemap=False)
     def web_auth_signup(self, *args, **kw):
-        return request.redirect('/') 
+        return  werkzeug.utils.redirect('/')
